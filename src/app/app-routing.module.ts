929ed4d425c38864eef33580b1583cbb4026c51c
Volodymyr Polishchuk <volodymyrpo@tahometer.com>
2020-06-12 01:10:49 +0300

    Add tabs and pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PersonalQualitiesComponent} from './pages/personal-qualities/personal-qualities.component';
+import {PlansForTheFutureComponent} from './pages/plans-for-the-future/plans-for-the-future.component';
+import {
+  ProfessionalSkillsAndAbilitiesComponent
+} from './pages/professional-skills-and-abilities/professional-skills-and-abilities.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: PersonalQualitiesComponent,
+    path: 'personal-qualities',
+  },
+  {
+    component: ProfessionalSkillsAndAbilitiesComponent,
+    path: 'professional-skills-and-abilities',
+  },
+  {
+    component: PlansForTheFutureComponent,
+    path: 'plans-for-the-future',
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'personal-qualities'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppComponent } from './app.component';
 import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { PersonalQualitiesComponent } from './pages/personal-qualities/personal-qualities.component';
+import { ProfessionalSkillsAndAbilitiesComponent } from './pages/professional-skills-and-abilities/professional-skills-and-abilities.component';
+import { PlansForTheFutureComponent } from './pages/plans-for-the-future/plans-for-the-future.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonalQualitiesComponent,
+    ProfessionalSkillsAndAbilitiesComponent,
+    PlansForTheFutureComponent,
   ],
   imports: [
     BrowserModule,
