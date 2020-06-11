e0a725e244de619d621d416d8f05cd4d24d75252
Volodymyr Polishchuk <volodymyrpo@tahometer.com>
2020-06-12 01:28:38 +0300

    Add pages titles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {environment} from '../environments/environment';
+import {NavigationEnd, Router} from '@angular/router';
+import {filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +11,19 @@ import {environment} from '../environments/environment';
 })
 export class AppComponent implements OnInit {
   title = 'Web Technology';
+  currentUrl: string;
   private lang = 0;
 
-  constructor(private translateService: TranslateService) {}
+  constructor(private translateService: TranslateService,
+              private router: Router) {}
 
   ngOnInit(): void {
     this.translateService.use(environment.locales[1]);
+    this.router.events.pipe(
+      filter(value => value instanceof NavigationEnd)
+    ).subscribe((value: any) => {
+      this.currentUrl = value.url;
+    });
   }
 
   selectNextLang() {
